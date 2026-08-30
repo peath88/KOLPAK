97c50900ce517d431f44c7a8d1e24ebd2ba41a74
peath88 <dima.semin2001@mail.ru>
2026-08-30 18:28:38 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="KOLPAK",
+  version="1.0.1",
+  description="VK id parser",
+  author="peath88",
+  packages=find_packages(),
+  install_requires=[
+    "vk_api>=11.9.0",
+  ],
+  entry_points={
+    "console_scripts": [
+      "KOLPAK=KOLPAK:main",
+    ],
+  },
+  python_requires=">=3.6",
+)
